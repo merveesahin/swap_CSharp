f399eb6074b6a389ef8d7ee52fa70c44a405dc38
merve <merve.nur14@outlook.com>
2022-07-07 15:01:36 +0300

    Add project files.

---
+++ swap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace swap
+{
+    class Program
+    {
+        public static void takas(int a, int b)
+        {
+            int t;
+            t = a;
+            a = b;
+            b = t;
+        }
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+
+            Console.WriteLine("\n" + "a:" +a);
+            Console.WriteLine("\n" + "b:" + b);
+        }
+    }
+}
